9b95e6292f3247382904d828d9edf10eb9b0ec25
kat d <katerinadungog@gmail.com>
2023-08-14 09:26:51 -0700

    updated footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <footer className="navbar py-6 relative flex items-center w-full justify-between">
       <div className="px-6 w-full flex flex-wrap items-center justify-between">
-        <div className="flex flex-col-reverse md:flex-row items-center w-full">
+        <div className="flex flex-col-reverse md:flex-row items-center justify-end w-full md:text-right">
           <span className="text-sm sm:text-center text-cyan dark:text-medium-cyan">
             &copy; {new Date().getFullYear().toString()} vvvaporwaves. All Rights
             Reserved.
