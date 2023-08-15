2852717a8f31209f6f3742c312a9a3e2dd5f58ab
Jay Chowdhary <jayc.chowdhary@gmail.com>
2023-08-14 20:18:10 -0700

    Auto-generated project pages.

---
+++ src/templates/project-page.tsx
@@ -0,0 +1,69 @@
+import React, { useState } from 'react';
+import { graphql, HeadFC, HeadProps, Link, PageProps } from 'gatsby';
+
+import SEO from '../components/seo';
+import CommentModal from '../components/blog/comment-modal';
+
+type ContextProps = {
+  id: string;
+};
+
+type DataProps = {
+  project: {
+    html: string;
+    frontmatter: {
+      title: string;
+      subtitle: string;
+      preview_url: string;
+    };
+  };
+};
+
+const ProjectPage = ({
+  data: { project },
+  pageContext
+}: PageProps<DataProps, ContextProps>) => {
+  return (
+    <div className="flex flex-col w-full my-10 mx-6 sm:mx-0 items-center">
+      <div className="flex w-full items-center">
+        <h2 className="text-4xl font-semibold uppercase text-white text-center">
+          {project.frontmatter.title}
+        </h2>
+        <span className="h-0.5 flex-1 ml-6 bg-white" />
+      </div>
+
+      <div class="mockup-window border bg-[#EC77A8] mt-4 self-start">
+        <div class="bg-[#141315] border-t">
+          <iframe
+            src={project.frontmatter.preview_url}
+            title="fireflower"
+          ></iframe>
+        </div>
+      </div>
+
+      <article
+        className="flex w-full flex-col blog-post text-white gap-3 my-10"
+        style={{ maxWidth: 1000 }}
+        dangerouslySetInnerHTML={{ __html: project.html }}
+      />
+    </div>
+  );
+};
+
+export const pageQuery = graphql`
+  query ($id: String!) {
+    project: markdownRemark(id: { eq: $id }) {
+      html
+      frontmatter {
+        title
+        subtitle
+        preview_url
+      }
+    }
+  }
+`;
+
+export default ProjectPage;
+export const Head: HeadFC<DataProps> = (props: HeadProps<DataProps>) => (
+  <SEO title={props.data.project.frontmatter.title} />
+);
