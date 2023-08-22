d2979dba9e1e0bbbf7c101cf1924fe72428d0c55
Jay Chowdhary <jayc.chowdhary@gmail.com>
2023-08-21 17:48:27 -0700

    Added mobile menu.

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,15 +3,41 @@ import { OutboundLink } from 'gatsby-plugin-google-gtag';
 
 import { FaEnvelope, FaGithub, FaCodepen, FaLinkedin } from 'react-icons/fa';
 import { Link } from 'gatsby';
+import Menu from './menu';
 
 const Header = () => {
   const [sideBarOpen, setSideBarOpen] = useState(false);
 
   return (
     <nav className="navbar py-6 relative flex items-center w-full justify-between bg-black">
+      <Menu isOpen={sideBarOpen} setOpen={setSideBarOpen} />
+
       <div className="px-6 w-full flex flex-wrap items-center justify-between">
         <div className="flex w-full items-center">
-          <ul className="navbar-nav flex text-purple">
+          <button
+            className="items-center justify-center relative nav-item icon text-purple flex md:hidden"
+            onClick={() => setSideBarOpen(true)}
+          >
+            <span
+              className={`transform transition w-full h-px bg-current absolute ${
+                sideBarOpen ? 'translate-y-0 rotate-45' : '-translate-y-2'
+              }`}
+            />
+
+            <span
+              className={`transform transition w-full h-px bg-current absolute ${
+                sideBarOpen ? 'opacity-0 translate-x-3' : 'opacity-100'
+              }`}
+            />
+
+            <span
+              className={`transform transition w-full h-px bg-current absolute ${
+                sideBarOpen ? 'translate-y-0 -rotate-45' : 'translate-y-2'
+              }`}
+            />
+          </button>
+
+          <ul className="navbar-nav text-purple hidden md:flex">
             <li className="nav-item">
               <Link to="/" activeClassName="text-pink">
                 home
@@ -23,7 +49,11 @@ const Header = () => {
               </Link>
             </li>
             <li className="nav-item ml-5">
-              <Link to="/portfolio" activeClassName="text-pink">
+              <Link
+                to="/portfolio"
+                partiallyActive={true}
+                activeClassName="text-pink"
+              >
                 portfolio
               </Link>
             </li>
